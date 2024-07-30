fba6ce3f9cf207509924267e0b103b4c581f7bb6
sofyankirat <www.sofyankirat123@gmail.com>
2024-07-30 15:44:05 -0700

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-const process = require('process');
-const fs = require('fs');
 
-// The first argument is the file path
+const fs = require('fs');
 const file = process.argv[2];
-// The content of the file must be written in utf-8
-fs.readFile(file, 'utf8', function (err, data) {
-  // If an error occurred during the reading, print the error object
+
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
