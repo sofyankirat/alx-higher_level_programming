a9896461dcc7d55da01b0f308df8b85ec5fd27e7
sofyankirat <www.sofyankirat123@gmail.com>
2024-07-30 15:18:38 -0700

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
+const filePath = process.argv[2];
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(filePath, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
