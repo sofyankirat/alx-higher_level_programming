c0f5f9719b11ba6e6ff9c0569b1778c6c28c0099
sofyankirat <www.sofyankirat123@gmail.com>
2024-07-30 15:38:15 -0700

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-// Reads and prints the content of a file
+const fs = require('fs');
 
-const argv = process.argv;
-let fs = require('fs');
-fs.readFile(argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });
