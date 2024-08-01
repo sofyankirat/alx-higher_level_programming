e854af65147a98c1c0a7b68ff8f71255bac3a873
sofyankirat <www.sofyankirat123@gmail.com>
2024-08-01 16:23:52 -0700

    sdc

--- 0x0A-python-inheritance/0-lockup.py
+++ 0x0A-python-inheritance/0-lockup.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-# 0-lookup.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines an object attribute lookup function."""
+'''0-lookup.py
+'''
 
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))
+    """ returns the list of available attributes and methods of an object """
+    return dir(obj)
