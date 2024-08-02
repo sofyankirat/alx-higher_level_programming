958e944b8be48bcb5e0f2fa3bf6a3ca696baf83a
sofyankirat <www.sofyankirat123@gmail.com>
2024-08-01 20:02:32 -0700

    csdc

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database hbtn_0e_14_usa"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+"""Defines a class City"""
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-        .order_by(City.id).all()
-    for city, state in rows:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """Module of a city"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
